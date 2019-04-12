1ffe76bb505512551de5b0a1ce394c586ba67e6d
Ivan-Davila <arkangel_ivan@hotmail.com>
2019-04-12 14:12:22 -0500

    BD 1.1

---
+++ LawnTree/LawnTree/Clases/Conexion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Data;
+using System.Windows.Forms;
+
+namespace LawnTree.Clases
+{
+    class Conexion
+    {
+        SqlConnection conectar;
+        SqlCommand cmd = new SqlCommand();
+
+        public void insertar()
+        {
+            conectar = new SqlConnection();
+            conectar.ConnectionString = conexion_string.string_conexion();
+            try
+            { 
+                conectar.Open();
+                cmd.Connection = conectar;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "insertar";
+
+                cmd.Parameters.Add("@FirstName", SqlDbType.VarChar,50);
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+        }
+    }
+}
---
+++ LawnTree/LawnTree/Clases/conexion_string.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using LawnTree.Properties;
+
+namespace LawnTree.Clases
+{
+    class conexion_string
+    {
+        public static string string_conexion()
+        {
+            return Settings.Default.LawnTreeBDConnectionString;
+        }
+    }
+}
